91c175d20f0933ccd77bce8edc5d3a38f30a7c92
DaniAm94 <dani.amico94@gmail.com>
2024-02-23 16:41:20 +0100

    added html layout to show the movies list

--- index.php
+++ index.php
@@ -14,6 +14,10 @@ class Director{
       $this->first_production= $first_production;
       $this->last_production= $last_production;
    }
+
+   function getFullName(){
+      return "$this->first_name $this->last_name";
+   }
 }
 
 $chris_columbus= new Director('Chris', 'Columbus', 68,'Tutto quella notte', 'Qualcuno salvi il natale 2' );
@@ -31,6 +35,9 @@ class Actor{
       $this->age= $age;
       $this->origin= $origin;
       $this->debut= $debut;
+   }
+   function getFullName(){
+      return "$this->first_name $this->last_name";
    }
 }
 $daniel_radcliffe= new Actor('Daniel', 'Radcliffe', 35, 'England', 1999);
@@ -64,13 +71,57 @@ $harry_potter->addCastMember($daniel_radcliffe);
 $harry_potter->addCastMember($ruper_grint);
 $harry_potter->addCastMember($emma_watson);
 $harry_potter->setDirector($chris_columbus);
-var_dump($harry_potter);
 
 $harry_potter_2= new Movie('Harry Potter and the chamber of secrets', $plot, 'english');
 $harry_potter_2->addCastMember($daniel_radcliffe);
 $harry_potter_2->addCastMember($ruper_grint);
 $harry_potter_2->addCastMember($emma_watson);
 $harry_potter_2->setDirector($chris_columbus);
-var_dump($harry_potter_2);
 
+
+$movies[]=$harry_potter;
+$movies[]=$harry_potter_2;
 ?>
+
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP Movie</title>
+
+   <!-- Bootstrap -->
+   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+</head>
+<body>
+   <div class="container-sm">
+      <header class="py-3">
+         <h1 class="text-center">My movies - PHP</h1>
+      </header>
+      <main>
+         <ul class="list-unstyled row row-cols-2">
+            <?php foreach($movies as $movie) : ?>
+               <li class="col">
+               <div class="card m-auto " style="width: 18rem;">
+                 <div class="card-body">
+                   <h5 class="card-title text-info"><?= $movie->title ?></h5>
+                   <hr>
+                   <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-danger">Director:</span> <?= $movie->director->getFullName() ?></h6>
+                   <h6 class="text-danger">Cast:</h6>
+                   <ul> 
+                      <?php foreach($movie->cast as $actor):?>
+                     <li><?= $actor->getFullName() ?></li>
+                      <?php endforeach?>
+                   </ul>
+                   <hr>
+                   <p class="card-text text-warning "><?= $movie->plot ?></p>
+                 </div>
+               </div>
+               </li>
+               <?php endforeach ?>
+         </ul>
+      </main>
+   </div>
+   
+</body>
+</html>
